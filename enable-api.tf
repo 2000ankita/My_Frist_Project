e450f80356fff5606bb5aed7a9a8e286d5bf3ffe
Ankita Vakil <ankitasvakil@gmail.com>
2025-03-09 13:52:17 +0530

    Enabling the api-fix variable crendentials to use the cloud variable-using locals

--- enable-api.tf
+++ enable-api.tf
@@ -1,4 +1,13 @@
-resource "google_project_service" "compute" {
+locals {
+  apis_to_enable = [
+    "cloudresourcemanager.googleapis.com",
+    "compute.googleapis.com"
+  ]
+}
+
+resource "google_project_service" "api_services" {
+  for_each = toset(local.apis_to_enable)
+
   project = var.project_id
-  service = "compute.googleapis.com"
+  service = each.value
 }
